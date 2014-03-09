9aa6b784ff5abcab5808e963fd2218725ae551e8
David Perkalis <dperkalis@hotmail.com>
2014-03-08 22:50:12 -0500

    Added new pages.

--- app/js/controllers/controllers.js
+++ app/js/controllers/controllers.js
@@ -3,37 +3,37 @@
 /* Controllers */
 
 angular.module('ppApp.controllers', [])
-.controller('AddItemCtrl', ['$scope', function($scope) {
-        $scope.hello = function() {
+    .controller('AddItemCtrl', ['$scope', function ($scope) {
+        $scope.hello = function () {
             alert('hi');
         };
 
-        $scope.submitForm = function(isValid) {
+        $scope.submitForm = function (isValid) {
             alert('submitting form' + isValid);
         };
-}])
-.controller('SearchItemCtrl', [function() {
+    }])
+    .controller('SearchItemCtrl', [function () {
 
-}])
-.controller('SummaryCtrl', ['$scope', 'EntryService',
-    function($scope, EntryService) {
-        $scope.entries = [];
-        var promise = EntryService.getSummary();
-        promise.then(function(response) {
-            $scope.entries = response.data;
-        },
-        function(errorResponse) {
-            alert(errorResponse);
-        });
-}])
-.controller('JournalCtrl', ['$scope', 'EntryService',
-        function($scope, EntryService) {
+    }])
+    .controller('SummaryCtrl', ['$scope', 'EntryService',
+        function ($scope, EntryService) {
+            $scope.entries = [];
+
+            EntryService.getSummary().then(function (response) {
+                    $scope.entries = response;
+                },
+                function (errorResponse) {
+                    alert(errorResponse);
+                });
+        }])
+    .controller('JournalCtrl', ['$scope', 'EntryService',
+        function ($scope, EntryService) {
             $scope.journal = {};
-            var promise = EntryService.getJournalDetail();
-            promise.then(function(response) {
-                $scope.journal = response.data;
-            },
-            function(errorResponse) {
-                alert(errorResponse);
-            });
+
+            EntryService.getJournalDetail().then(function (response) {
+                    $scope.journal = response.data;
+                },
+                function (errorResponse) {
+                    alert(errorResponse);
+                });
         }]);
--- app/js/services/services.js
+++ app/js/services/services.js
@@ -2,33 +2,33 @@
 
 /* Services */
 angular.module('ppApp.services', [])
-    .factory('EntryService', ['$http', '$q', function($http, $q) {
+    .factory('EntryService', ['$http', '$q', function ($http, $q) {
 
 
-        var getSummary = function() {
-            var deferred = $q.defer();
-            $http({method : "GET", url :"data/summary.json"}).success(function(data, status, headers, config) {
-                deferred.resolve(data);
-            }).error(function(data, status, headers, config) {
-                   deferred.reject(status)
-            });
+        var getSummary = function () {
+                var deferred = $q.defer();
+                $http({method: "GET", url: "data/summary.json"}).success(function (data, status, headers, config) {
+                    deferred.resolve(data);
+                }).error(function (data, status, headers, config) {
+                        deferred.reject(status)
+                    });
 
-            return deferred.promise;
+                return deferred.promise;
+            },
+
+            getJournalDetail = function () {
+                var deferred = $q.defer();
+                return $http({method: "GET", url: "data/journal.json"}).success(function (data, status, headers, config) {
+                    deferred.resolve(data);
+                }).error(function (data, status, headers, config) {
+                        deferred.reject(status);
+                    });
+
+                return deferred.promise;
+            }
+
+        return {
+            getSummary: getSummary,
+            getJournalDetail: getJournalDetail
         };
-
-        var getJournalDetail = function() {
-            var deferred = $q.defer();
-            return $http({method: "GET", url: "data/journal.json"}).success(function(data, status, headers, config) {
-                deferred.resolve(data);
-            }).error(function(data, status, headers, config) {
-                    deferred.reject(status);
-                });
-
-            return deferred.promise;
-        }
-
-    return {
-        getSummary : getSummary,
-        getJournalDetail : getJournalDetail
-    };
-}]);
+    }]);
